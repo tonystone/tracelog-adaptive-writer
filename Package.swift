e3290ae0bd2da6c61bee9be45a3183f07eb3873e
Tony Stone <hatter24@gmail.com>
2018-05-27 10:32:06 -0700

    Initial commit of test project.

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:4.0
+///
+/// Package.swift
+///
+/// Created by Tony Stone on 5/27/18.
+///
+import PackageDescription
+
+let package = Package(
+        name: "TraceLogJournalWriter",
+        products: [
+            .executable(name: "tester", targets: ["TraceLogJournalWriter"])
+        ],
+        dependencies: [
+            .package(url: "git@github.com:felix91gr/Csdjournal.git", .exactItem("0.9.0"))
+        ],
+        targets: [
+            /// Module targets
+            .target(name: "TraceLogJournalWriter", dependencies: [], path: "Sources/TraceLogJournalWriter")
+        ],
+        swiftLanguageVersions: [4]
+)
